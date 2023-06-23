53f41f727d3524ec50c7e9edef156d1f918d89f3
pinkmins2 <pinkmins2@gmail.com>
2023-06-23 13:34:21 +0900

    인사말 기능을 인터페이스 기반으로 작성

---
+++ src/main/java/hello/spring/bean/Spring5Bean03.java
@@ -0,0 +1,7 @@
+package hello.spring.bean;
+
+public interface Spring5Bean03 {
+    // 인사말을 하는 기능을하는 클래스를 만들기 위한 규약
+    // 인사말을 출력하는 메서드 정의
+    void sayHello(String msg);
+}
---
+++ src/main/java/hello/spring/bean/Spring5Bean03Eng.java
@@ -0,0 +1,9 @@
+package hello.spring.bean;
+
+public class Spring5Bean03Eng implements Spring5Bean03{
+
+    public void sayHello(String msg) {
+        System.out.println("헬로, " +msg);
+    }
+
+}
