5be997f0b8e5c861dfcb4633300dc197e5c47de2
pinkmins2 <pinkmins2@gmail.com>
2023-06-23 11:03:29 +0900

    Spring 첫번째 사례

---
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -0,0 +1,12 @@
+package hello.spring.basic;
+
+public class Spring5App00 {
+    // 첫번재 사례
+    // 간단한 인사말을 출력하는 프로그램 작성
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+
+}
