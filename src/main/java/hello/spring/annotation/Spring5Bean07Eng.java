a1b26f84956882dad15bdcd4a018d675784b5de2
pinkmins2 <pinkmins2@gmail.com>
2023-06-23 15:20:08 +0900

    애너테이션을 이용한 인사말 빈 생성

--- src/main/java/hello/spring/annotation/Spring5Bean07Eng.java
+++ src/main/java/hello/spring/annotation/Spring5Bean07Eng.java
@@ -4,7 +4,6 @@ import hello.spring.bean.Spring5Bean03;
 import org.springframework.stereotype.Component;
 
 @Component("eng")
-
 public class Spring5Bean07Eng implements Spring5Bean03 {
 
     public void sayHello(String msg) {
--- src/main/java/hello/spring/annotation/Spring5Bean07Jpn.java
+++ src/main/java/hello/spring/annotation/Spring5Bean07Jpn.java
@@ -4,7 +4,6 @@ import hello.spring.bean.Spring5Bean03;
 import org.springframework.stereotype.Component;
 
 @Component("jpn")
-
 public class Spring5Bean07Jpn implements Spring5Bean03 {
 
     public void sayHello(String msg) {
